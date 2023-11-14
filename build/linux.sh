d5ad9ec3486864a327db7f2fc03c180f95bada50
FrancoBarrera99 <francototogomez@gmail.com>
2023-11-13 22:43:14 -0300

    entorno de desarrollo linux funcionando

--- build/linux.sh
+++ build/linux.sh
@@ -28,7 +28,25 @@ mv lex.yy.c $INTERMEDIATE_FOLDER/
 mv traductor.tab.c $INTERMEDIATE_FOLDER/
 mv traductor.tab.h $INTERMEDIATE_FOLDER/
 
+# Crear la carpeta binaries si no existe
+mkdir -p $DESTINATION_FOLDER
+
 # Compilación con gcc
 gcc -Wall -o $DESTINATION_FOLDER/$EXECUTABLE_NAME $INTERMEDIATE_FOLDER/lex.yy.c $INTERMEDIATE_FOLDER/traductor.tab.c
 
 echo "Compilación completada."
+
+# Generar el archivos de tests en la carpeta bin
+echo "#!/bin/bash" > $DESTINATION_FOLDER/run_test1.sh
+echo "./$EXECUTABLE_NAME < $TESTS_FOLDER/test1.txt" >> $DESTINATION_FOLDER/run_test1.sh
+chmod +x $DESTINATION_FOLDER/run_test1.sh
+
+echo "#!/bin/bash" > $DESTINATION_FOLDER/run_test2.sh
+echo "./$EXECUTABLE_NAME < $TESTS_FOLDER/test2.txt" >> $DESTINATION_FOLDER/run_test2.sh
+chmod +x $DESTINATION_FOLDER/run_test2.sh
+
+echo "#!/bin/bash" > $DESTINATION_FOLDER/run_test3.sh
+echo "./$EXECUTABLE_NAME < $TESTS_FOLDER/test3.txt" >> $DESTINATION_FOLDER/run_test3.sh
+chmod +x $DESTINATION_FOLDER/run_test3.sh
+
+echo "Se han generado los binarios de prueba"
